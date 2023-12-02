b110f77c3a6065b290b599983029db235d6e399c
Blthrst <d.user@internet.ru>
2023-12-02 22:28:53 +0700

    feat: delete & put controllers

--- crud-app/src/crud/crud.controller.ts
+++ crud-app/src/crud/crud.controller.ts
@@ -1,17 +1,39 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
-import { CrudService } from './crud.service';
+import { Controller, Delete, Get, Post, Query, Req, Res } from "@nestjs/common";
+import { CrudService } from "./crud.service";
+import { Request, Response } from "express";
 
-@Controller('api')
+@Controller("api")
 export class CrudController {
   constructor(private readonly crudService: CrudService) {}
 
-  @Get('get')
+  @Get("get")
   returnData(): Array<Object> {
     return this.crudService.returnData();
   }
 
-  @Post('post')
-  postData(@Query('times') times: number): void {
-    console.log( this.crudService.postData(times) )
+  @Post("post")
+  postData(@Req() req: Request, @Res() res: Response): void {
+    const times: number = parseInt(req.body.postField);
+    console.log(this.crudService.postData(times));
+    res.redirect("/");
+  }
+
+  @Post("delete")
+  deleteData(@Req() req: Request, @Res() res: Response): void {
+    const id: number = parseInt(req.body.deleteField);
+    console.log(this.crudService.deleteData(id));
+    res.redirect("/");
+  }
+
+  @Post("put")
+  updateData(@Req() req: Request, @Res() res: Response): void {
+    const { updateFieldOldId, updateFieldNewId } = req.body
+    console.log(
+      this.crudService.updateData(
+        parseInt(updateFieldOldId),
+        parseInt(updateFieldNewId),
+      ),
+    );
+    res.redirect("/");
   }
 }
--- crud-app/src/crud/crud.service.ts
+++ crud-app/src/crud/crud.service.ts
@@ -1,23 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+
+export interface TestObject {
+  id: number,
+  name: string,
+  [key: string]: any
+}
 
 @Injectable()
 export class CrudService {
-  returnData(): Array<Object> {
-    const result: Array<Object> = [];
+  returnData(): Array<TestObject> {
+    const result: Array<TestObject> = [];
 
     for (let i = 0; i < 10; i++) {
-      result.push({id: i, age: 10+i, name: "person_"+i});
+      result.push({ id: i, age: 10 + i, name: "person_" + i });
     }
 
-    return result
+    return result;
   }
 
-  postData(times: number): Array<Object> {
-    const result: Array<Object> = []
+  postData(times: number): Array<TestObject> {
+    const result: Array<TestObject> = [];
 
     for (let i = 0; i < times; i++) {
-        result.push({id: i, name: "object_"+i})
+      result.push({ id: i, name: "object_" + i });
     }
-    return result
+    return result;
+  }
+
+  deleteData(id: number): Array<TestObject> {
+    const result: Array<TestObject> = [];
+
+    for (let i = 0; i < id + 5; i++) {
+      result.push({ id: i, name: "object_" + i });
+    }
+
+    for (let obj of result) {
+      if (obj.id == id) {
+        result.splice(result.indexOf(obj), 1);
+      }
+    }
+
+    return result;
+  }
+
+  updateData(id: number, newId: number): Array<TestObject> {
+    const result: Array<TestObject> = [];
+
+    for (let i = 0; i < id + 5; i++) {
+      result.push({ id: i, name: "object_" + i });
+    }
+
+    for (let obj of result) {
+      if (obj.id == id) {
+        obj.id = newId
+      }
+    }
+
+    return result;
   }
 }
