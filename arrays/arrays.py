f230bf299efc2071512d23f9f9d22ce70172563e
Blthrst <d.user@internet.ru>
2023-12-22 22:33:26 +0700

    refactor: arrays.py

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -1,14 +1,14 @@
-list = list([x for x in range(10) if x > 4])
+# list = list([x for x in range(10) if x > 4])
 
-list.pop()
+# list.pop()
 
-list.insert(0, 0)
+# list.insert(0, 0)
 
-list[1:2] = [100]
+# list[1:2] = [100]
 
-list.append(999)
+# list.append(999)
 
-sorted_list = sorted(list)
+# sorted_list = sorted(list)
 
 #print(sorted_list, "\n", list, f"\nMAX: {max(list)}\nMIN: {min(list)}\n\nEXTENDED: {list + sorted_list}")
 
@@ -18,9 +18,31 @@ original_set = set([1, 1, 0, 0, 1])
 another_set = set([2, 0, 3, 5, 6, 6])
 
 united_set = original_set.union(another_set)
-
+#22:34
 #print(united_set)
 #print(original_set.intersection(another_set))
 #print(original_set.difference(another_set))
 #print(original_set.symmetric_difference(another_set))
 #print({x for x in ["test", "test", 1, 2, 3, 3, 5, 6, 6]})
+
+
+############################################################################
+
+#list = [[0, 1], [2, 3], [3, 4]]
+
+# for first, second in list:
+#     print(f"First element: {first}\nSecond element: {second}")
+
+*unpacked, = [1, 2, 3, 4, 5]
+
+origin = dict([[1, 2]])
+extension = dict([[4, 5]])
+
+
+#print({**origin, **extension})
+
+def kwargs(name, age, **kwargs):
+    print(name, age)
+    print(kwargs)
+obj = {"name":"test_name", "age":"test_age", "wtf": "wtf"}
+kwargs(**obj)
